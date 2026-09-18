69a12880625cab391ec7c33e5c732c4de762e90a
IsaiasLeonel00012 <isaiasleonel2004@gmail.com>
2026-09-17 21:29:53 -0600

    gradiente geometrico

--- modulos/gradiente_geometrico.py
+++ modulos/gradiente_geometrico.py
@@ -1,1 +1,15 @@
-print("hola mundo")
+def calcular_gradiente_geometrico(pago, tasa, crecimiento, periodos):
+    if tasa == crecimiento:
+        valor_presente = (pago * periodos) / (1 + tasa)
+    else:
+        valor_presente = pago * (
+            1 - ((1 + crecimiento) / (1 + tasa)) ** periodos
+        ) / (tasa - crecimiento)
+
+    flujos = []
+
+    for periodo in range(1, periodos + 1):
+        flujo = pago * (1 + crecimiento) ** (periodo - 1)
+        flujos.append(flujo)
+
+    return valor_presente, flujos
