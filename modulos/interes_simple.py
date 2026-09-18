c66b6880cbe924f52df24a76ee078a80b1d97d0f
IsaiasLeonel00012 <isaiasleonel2004@gmail.com>
2026-09-17 20:46:27 -0600

    interes simple

--- modulos/interes_simple.py
+++ modulos/interes_simple.py
@@ -1,1 +1,5 @@
-print("hola mundo")
+def calcular_interes_simple(capital, tasa, periodos):
+    interes = capital * tasa * periodos
+    valor_futuro = capital * (1 + tasa * periodos)
+
+    return interes, valor_futuro
